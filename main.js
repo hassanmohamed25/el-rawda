bd370b925ad3dbadc41bda94ffdb8aebafae710b
hassanmohamed25 <131494115+hassanmohamed25@users.noreply.github.com>
2023-04-22 04:16:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+let navigation =document.querySelector('.navigation-2');
+    document.querySelector('.toggle').onclick =  function
+    (){
+        this.classList.toggle('active');
+        navigation.classList.toggle('active');
+    }
+
+
